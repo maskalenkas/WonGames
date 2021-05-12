3baa1bebe3f728f3b4998d6482b854e55b7125bf
Arthur <maskalenkasth@gmail.com>
2021-05-11 23:29:32 -0300

    button com testes finalizado

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,26 @@
+import React from 'react';
 import * as S from './styles';
 
 export type ButtonProps = {
   children?: React.ReactNode;
   size?: 'small' | 'medium' | 'large';
+  fullWidth?: boolean;
+  icon?: JSX.Element;
+  onClick?: () => (event: React.MouseEvent<HTMLButtonElement>) => void;
 };
 
-const Button = ({ children, size = 'medium' }: ButtonProps) => (
-  <S.Wrapper size={size}>{!!children && <span>{children}</span>}</S.Wrapper>
+// Se eu não passar um icone, o hasIcon vai chegar como false
+const Button = ({
+  children,
+  icon,
+  size = 'medium',
+  fullWidth = false,
+  ...props
+}: ButtonProps) => (
+  <S.Wrapper size={size} fullWidth={fullWidth} hasIcon={!!icon} {...props}>
+    {icon}
+    {!!children && <span>{children}</span>}
+  </S.Wrapper>
 );
 
 export default Button;
