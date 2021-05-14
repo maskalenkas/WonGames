d6056ea070a9d1ce9f16e0e2bf2b43be5cd3c26c
Arthur <maskalenkasth@gmail.com>
2021-05-14 03:50:15 -0300

    styled movido

--- styled.components.d.ts
+++
@@ -1,9 +0,0 @@
-import theme from 'styles/theme';
-
-// inferência de tipos, ao invés de declarar cada tipo
-type Theme = typeof theme;
-
-declare module 'styled-components' {
-  // eslint-disable-next-line @typescript-eslint/no-empty-interface
-  export interface DefaultTheme extends Theme {}
-}
