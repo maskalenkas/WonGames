ac2441cb0deb3b6fdaa57b74be1c33e6ed222bef
Arthur <maskalenkasth@gmail.com>
2021-05-21 02:18:13 -0300

    isso deve adicionar o componente container

---
+++ src/components/Container/test.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from '@testing-library/react';
+import theme from 'styles/theme';
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import { Container } from '.';
+
+describe('<Container />', () => {
+  it('should render the heading', () => {
+    const { debug, container } = renderWithTheme(<Container></Container>);
+
+    expect(container.firstChild).toHaveStyleRule(
+      'max-width',
+      theme.grid.container,
+    );
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      .c0 {
+        max-width: 130rem;
+        margin-left: auto;
+        margin-right: auto;
+        padding-left: calc(3.2rem);
+        padding-right: calc(3.2rem);
+      }
+
+      <div
+        class="c0"
+      />
+    `);
+  });
+});
