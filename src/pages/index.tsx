4f15b41fa6882963e8199be40320f5f40ba2d10c
Arthur <maskalenkasth@gmail.com>
2021-05-23 03:24:48 -0300

    isso deve criar um index de teste

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,18 @@
 import Home from 'templates/Home';
 
-export default function Index() {
-  return <Home />;
+export default function Index({ heading }: any) {
+  return <Home {...heading} />;
+}
+
+export function getStaticProps() {
+  // faz lógica como:
+  // pode buscar dados numa API
+  // fazer calculo|leitura de context
+
+  // retorno dos dados
+  return {
+    props: {
+      heading: 'Um heading',
+    },
+  };
 }
