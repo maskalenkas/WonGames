045800dad1b84dfb7e54dd4ba10f458ea66401cf
Arthur <maskalenkasth@gmail.com>
2021-05-22 22:44:05 -0300

    isso deve ajustar as coberturas dos testes

--- jest.config.js
+++ jest.config.js
@@ -2,7 +2,13 @@ module.exports = {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
+  collectCoverageFrom: [
+    'src/**/*.ts(x)?',
+    '!src/**/stories.tsx',
+    '!src/pages/**',
+    '!src/types/**',
+    '!src/styles/**',
+  ],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/', '<rootDir>/.jest'],
 };
