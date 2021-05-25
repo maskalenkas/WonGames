c540127a52720758c01f26c21910b1917df4cad3
Arthur <maskalenkasth@gmail.com>
2021-05-24 22:31:23 -0300

    isso deve adicionar mocks e configurar os stories para receber mocks

--- src/components/Highlight/stories.tsx
+++ src/components/Highlight/stories.tsx
@@ -1,16 +1,11 @@
 import { Story, Meta } from '@storybook/react/types-6-0';
 import Highlight, { HighlightProps } from '.';
+import mock from './mock';
 
 export default {
   title: 'Highlight',
   component: Highlight,
-  args: {
-    title: 'Read Dead is back!',
-    subtitle: 'Come see John’s new adventures',
-    backgroundImage: '/img/red-dead-img.jpg',
-    buttonLabel: 'Buy now',
-    buttonLink: '/games/rdr2',
-  },
+  args: { ...mock },
 } as Meta;
 
 export const Default: Story<HighlightProps> = (args) => (
