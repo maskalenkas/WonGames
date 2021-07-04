1e52096393b76895851c7bfe9b140b5968292eed
Maskalenkas <maskalenkasth@gmail.com>
2021-07-04 02:47:26 -0300

    isso deve adicionar a estrutura das paginas sign in sign out

--- src/templates/Auth/test.tsx
+++ src/templates/Auth/test.tsx
@@ -15,11 +15,25 @@ describe('<Auth />', () => {
     expect(screen.getAllByLabelText(/Won games/i)).toHaveLength(2);
 
     // Verifica se o heading principal foi renderizado
+    expect(
+      screen.getByRole('heading', { name: /all your favorite games/i }),
+    ).toBeInTheDocument();
 
     // Verifica se tem o subtitle
+    expect(
+      screen.getByRole('heading', {
+        name: /won is the best and most complete gaming platform/i,
+      }),
+    ).toBeInTheDocument();
 
     // Verifica se tem o title do content
+    expect(
+      screen.getByRole('heading', {
+        name: /sign out/i,
+      }),
+    ).toBeInTheDocument();
 
     // Verifica se o children esta sendo renderizado
+    expect(screen.getByRole('textbox')).toBeInTheDocument();
   });
 });
