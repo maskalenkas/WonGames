20be09f21e3576aacb79c5518ea66116a6d07d63
Maskalenkas <maskalenkasth@gmail.com>
2021-07-22 22:44:33 -0300

    pagina wishlist adicionada

---
+++ src/templates/Wishlist/index.tsx
@@ -0,0 +1,41 @@
+import Base from 'templates/Base';
+
+import Heading from 'components/Heading';
+import Showcase from 'components/ShowCase';
+import { Container } from 'components/Container';
+import GameCard, { GameCardProps } from 'components/GameCard';
+import { HighlightProps } from 'components/Highlight';
+
+import * as S from './styles';
+
+export type WishlistTemplateProps = {
+  games?: GameCardProps[];
+  recommendedGames: GameCardProps[];
+  recommendedHighlight: HighlightProps;
+};
+
+const Wishlist = ({
+  games,
+  recommendedGames,
+  recommendedHighlight,
+}: WishlistTemplateProps) => (
+  <Base>
+    <Container>
+      <Heading lineLeft lineColor="secondary">
+        Wishlist
+      </Heading>
+
+      {games?.map((game, index) => (
+        <GameCard key={`wishlist-${index}`} {...game} />
+      ))}
+    </Container>
+
+    <Showcase
+      title="You may like these games"
+      games={recommendedGames}
+      highlight={recommendedHighlight}
+    />
+  </Base>
+);
+
+export default Wishlist;
