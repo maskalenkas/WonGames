7fd338b458993302d20371d01c0c15d917728f24
Maskalenkas <maskalenkasth@gmail.com>
2021-07-23 22:35:58 -0300

    isso deve adicionar o divider

---
+++ src/components/Divider/test.tsx
@@ -0,0 +1,11 @@
+import { renderWithTheme } from 'utils/tests/helpers';
+
+import { Divider } from '.';
+
+describe('<Divider />', () => {
+  it('should render correctly', () => {
+    const { container } = renderWithTheme(<Divider />);
+
+    expect(container).toMatchSnapshot();
+  });
+});
